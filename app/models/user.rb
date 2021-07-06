a373e9d2717fea6cd1c14d34d2a6b9860a669a0e
Ubuntu <ubuntu@ip-172-31-30-127.us-east-2.compute.internal>
2021-07-06 06:32:27 +0000

    f

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
-  validates :name, :lastname,  presence: true
+ 
 end
